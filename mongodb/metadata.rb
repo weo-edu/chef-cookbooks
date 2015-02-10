91a6917002d39c6fa43266b6e670b98ac6626e36
Andrew Shaffer <andrew@weo.io>
2015-02-09 17:49:11 -0800

    try again

--- mongodb/metadata.rb
+++ mongodb/metadata.rb
@@ -13,7 +13,6 @@ recipe "mongodb::shard", "Installs and configures a single shard"
 recipe "mongodb::replicaset", "Installs and configures a mongodb replicaset"
 
 depends "apt", ">= 1.8.2"
-depends "apt"
 depends "python"
 depends "runit"
 depends "yum"
